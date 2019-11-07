163a24ffd1cb2ae59f908445749b0a66b82d70b3
Hectoraldrichsixx1313 <49257944+Hectoraldrichsixx1313@users.noreply.github.com>
2019-11-07 00:52:37 -0600

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const bodyParser = require("body-parser");
 
 // setup ports
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
 
 const base64URLEncode = (str) => {
